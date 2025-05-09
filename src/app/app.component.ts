b289c2d27f02eceb6e9ce5d78b8646d40d3fa317
Breno Cocheto <breno.cocheto@gmail.com>
2025-05-09 17:46:35 -0300

    feat(ui): add new transaction component
    - Created new transaction component
    - Added component to app.component.html
    - Implemented basic form structure
    - Styled the new component
    - Added select for transaction type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BannerComponent } from './components/banner/banner.component';
+import { NewTransactionComponent } from './components/new-transaction/new-transaction.component';
 
 @Component({
   selector: 'app-root',
-  imports: [BannerComponent],
+  imports: [BannerComponent, NewTransactionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })

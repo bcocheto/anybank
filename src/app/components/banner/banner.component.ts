8510be6df6dac24eec9d55e2b272d7afbf26a204
Breno Cocheto <breno.cocheto@gmail.com>
2025-05-09 17:35:06 -0300

    feat(banner): refactor banner component
    - Created separate components for welcome
    - and account sections
    - Improved component structure and styling
    - Added account balance display
    - Updated date formatting

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { AccountComponent } from './account/account.component';
 
 @Component({
   selector: 'app-banner',
-  imports: [],
+  imports: [WelcomeComponent, AccountComponent],
   templateUrl: './banner.component.html',
-  styleUrl: './banner.component.css'
+  styleUrl: './banner.component.css',
 })
-export class BannerComponent {
-
-}
+export class BannerComponent {}
